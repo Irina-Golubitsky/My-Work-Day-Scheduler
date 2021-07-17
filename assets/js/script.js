7e7b18bbce49c76af6ea0bd9ea20e2252810cbfe
Irina-Golubitsky <irina.golubitsky@gmail.com>
2021-07-17 09:30:06 -0700

    add comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,63 +1,65 @@
-// Moment.js
-let currentDate =moment().format("dddd, MMMM Do");
-let startHour=15;
-let workDayLength=9;
+// moment js
+let currentDate = moment().format("dddd, MMMM Do");
 $("#currentDay").text(currentDate);
-let list=[];
+// variables to set worday hours
+let startHour = 9; 
+let workDayLength = 9;
 // get or cleate list of events
-let day=localStorage.getItem('current-day')||'';
-if (day===currentDate){
-    list = JSON.parse(localStorage.getItem('timelist'));}
- else{
-    list=[];
-    for(i=0;i<workDayLength;i++){
+let list = [];
+let day = localStorage.getItem('current-day') || '';
+if (day === currentDate) {
+    list = JSON.parse(localStorage.getItem('timelist'));
+}
+else {
+    list = [];
+    for (i = 0; i < workDayLength; i++) {
         list.push('');
     }
- }
- // show grid
- let currenHour=moment().hour();
- console.log(currenHour);
- for (i=0;i<workDayLength;i++){
-     let rowDiv=$('<div>');
-     rowDiv.addClass('row');
-     
-     // show hour
-     let hourDiv=$('<div>');
-     hourDiv.addClass('hour col-1 text-center');
-     hourDiv.text(moment(i+startHour, ["H"]).format("h A"));
-     rowDiv.append(hourDiv);
-     // show event
-     let eventArea=$('<textarea>');
-     eventArea.attr('id', 'text'+i);
-     if ((i+startHour)<currenHour){
-         eventArea.addClass('past col-10 ');
-     }else if((i+startHour)===currenHour){
+}
+// show timeblocks
+let currenHour = moment().hour();
+for (i = 0; i < workDayLength; i++) {
+    let rowDiv = $('<div>');
+    rowDiv.addClass('row');
+    // show hour
+    let hourDiv = $('<div>');
+    hourDiv.addClass('hour col-1 text-center');
+    hourDiv.text(moment(i + startHour, ["H"]).format("h A"));
+    rowDiv.append(hourDiv);
+    // show event
+    let eventArea = $('<textarea>');
+        // add id for each textarea
+    eventArea.attr('id', 'text' + i);
+    if ((i + startHour) < currenHour) {
+        eventArea.addClass('past col-10 ');
+    } else if ((i + startHour) === currenHour) {
         eventArea.addClass('present col-10 ');
-     }else{
+    } else {
         eventArea.addClass('future col-10 ');
-     }
-     eventArea.text(list[i]);
-     rowDiv.append(eventArea);
-     // show save button
-     let saveBtn=$('<button>');
-     saveBtn.attr('data-list-item', i);
-     saveBtn.addClass("saveBtn col-1");
-     
-     let saveI=$('<i>');
-     saveI.addClass("fas fa-save");
-     saveBtn.append(saveI);
-     saveBtn.on('click', function(event){
-        event.preventDefault();
-       let cuurentRow=$(this).attr('data-list-item');
-        list[cuurentRow]=$('#text'+cuurentRow).val();
-      localStorage.setItem('timelist',JSON.stringify(list));
-      localStorage.setItem('current-day',currentDate);
-     });
-     
-     rowDiv.append(saveBtn);
-     
-     $('#container').append(rowDiv);
     }
-console.log(list);
+    eventArea.text(list[i]);
+    rowDiv.append(eventArea);
+    // show save button
+    let saveBtn = $('<button>');
+    saveBtn.attr('data-list-item', i);
+    saveBtn.addClass("saveBtn col-1");
+    let saveI = $('<i>');
+    saveI.addClass("fas fa-save");
+    saveBtn.append(saveI);
+        // add on.click function
+    saveBtn.on('click', function (event) {
+        event.preventDefault();
+        let cuurentRow = $(this).attr('data-list-item');
+        list[cuurentRow] = $('#text' + cuurentRow).val();
+            // update localStorage items
+        localStorage.setItem('timelist', JSON.stringify(list));
+        localStorage.setItem('current-day', currentDate);
+    });
+
+    rowDiv.append(saveBtn);
+
+    $('#container').append(rowDiv);
+}
+
 
 
